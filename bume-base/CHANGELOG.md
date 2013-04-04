1d6b0c46d3003749113721ddb345c77ed40f1078
Douglas Land <dsl@looprock.com>
2013-04-03 22:07:44 -0500

    added bume base to opsworks so I can actually use recipes from both

---
+++ bume-base/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for bume-appserver
+
+This file is used to list changes made in each version of bume-appserver.
+
+## 0.1.0:
+
+* Initial release of bume-appserver
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ bume-base/recipes/default.rb
@@ -0,0 +1,521 @@
+#
+# Cookbook Name:: bume-appserver
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# packaged generated via: for x in `cat ami_setup.sh |grep "yum -y install" |awk -F"install" {'print $2'}`; do echo "package \"$x\" do"; echo "     action [:install]"; echo "end"; done
+
+
+package "ant" do
+     action [:install]
+end
+package "antlr" do
+     action [:install]
+end
+package "apr" do
+     action [:install]
+end
+package "apr-util" do
+     action [:install]
+end
+package "aspell" do
+     action [:install]
+end
+package "aspell-en" do
+     action [:install]
+end
+package "audit-libs-python" do
+     action [:install]
+end
+package "autoconf" do
+     action [:install]
+end
+package "automake" do
+     action [:install]
+end
+package "avahi" do
+     action [:install]
+end
+package "avahi-glib" do
+     action [:install]
+end
+package "binutils" do
+     action [:install]
+end
+package "bzip2-devel" do
+     action [:install]
+end
+package "cairo" do
+     action [:install]
+end
+package "checkpolicy" do
+     action [:install]
+end
+package "cpp" do
+     action [:install]
+end
+package "cups-libs" do
+     action [:install]
+end
+package "libcurl-devel" do
+     action [:install]
+end
+package "cvs" do
+     action [:install]
+end
+package "dbus-glib" do
+     action [:install]
+end
+package "dbus-python" do
+     action [:install]
+end
+package "device-mapper-multipath" do
+     action [:install]
+end
+package "dmidecode" do
+     action [:install]
+end
+package "dos2unix" do
+     action [:install]
+end
+package "e2fsprogs-devel" do
+     action [:install]
+end
+package "expat-devel" do
+     action [:install]
+end
+package "gcc-c++" do
+     action [:install]
+end
+package "GeoIP" do
+     action [:install]
+end
+package "GeoIP-devel" do
+     action [:install]
+end
+package "gettext" do
+     action [:install]
+end
+package "gettext-devel" do
+     action [:install]
+end
+package "ghostscript" do
+     action [:install]
+end
+package "ghostscript-devel" do
+     action [:install]
+end
+package "ghostscript-fonts" do
+     action [:install]
+end
+package "glibc-devel" do
+     action [:install]
+end
+package "glibc-headers" do
+     action [:install]
+end
+package "gnutls" do
+     action [:install]
+end
+package "ImageMagick" do
+     action [:install]
+end
+package "ImageMagick-devel" do
+     action [:install]
+end
+package "imake" do
+     action [:install]
+end
+package "iptraf" do
+     action [:install]
+end
+package "iscsi-initiator-utils" do
+     action [:install]
+end
+package "java-1.6.0-openjdk-devel" do
+     action [:install]
+end
+package "kernel-headers" do
+     action [:install]
+end
+package "keyutils-libs-devel" do
+     action [:install]
+end
+package "krb5-devel" do
+     action [:install]
+end
+package "lcms" do
+     action [:install]
+end
+package "lcms-devel" do
+     action [:install]
+end
+package "libart_lgpl" do
+     action [:install]
+end
+package "libc-client" do
+     action [:install]
+end
+package "libdaemon" do
+     action [:install]
+end
+package "libfontenc" do
+     action [:install]
+end
+package "libFS" do
+     action [:install]
+end
+package "libgomp" do
+     action [:install]
+end
+package "libICE-devel" do
+     action [:install]
+end
+package "libIDL" do
+     action [:install]
+end
+package "libidn-devel" do
+     action [:install]
+end
+package "libjpeg-devel" do
+     action [:install]
+end
+package "libmcrypt" do
+     action [:install]
+end
+package "libselinux-devel" do
+     action [:install]
+end
+package "libselinux-python" do
+     action [:install]
+end
+package "libselinux-utils" do
+     action [:install]
+end
+package "libsemanage" do
+     action [:install]
+end
+package "libsepol-devel" do
+     action [:install]
+end
+package "libSM-devel" do
+     action [:install]
+end
+package "libstdc++-devel" do
+     action [:install]
+end
+package "compat-libtermcap" do
+     action [:install]
+end
+package "libtiff" do
+     action [:install]
+end
+package "libtiff-devel" do
+     action [:install]
+end
+package "libtool-ltdl" do
+     action [:install]
+end
+package "libusb" do
+     action [:install]
+end
+package "libwmf" do
+     action [:install]
+end
+package "libX11-devel" do
+     action [:install]
+end
+package "libXau-devel" do
+     action [:install]
+end
+package "libXaw" do
+     action [:install]
+end
+package "libXcursor" do
+     action [:install]
+end
+package "libXdmcp" do
+     action [:install]
+end
+package "libXdmcp-devel" do
+     action [:install]
+end
+package "libXext-devel" do
+     action [:install]
+end
+package "libXfixes" do
+     action [:install]
+end
+package "libXfont" do
+     action [:install]
+end
+package "libXft" do
+     action [:install]
+end
+package "libXinerama" do
+     action [:install]
+end
+package "libxml2-python" do
+     action [:install]
+end
+package "libXmu" do
+     action [:install]
+end
+package "libXpm" do
+     action [:install]
+end
+package "libXrandr" do
+     action [:install]
+end
+package "libXres" do
+     action [:install]
+end
+package "libxslt" do
+     action [:install]
+end
+package "libXt-devel" do
+     action [:install]
+end
+package "libXxf86vm" do
+     action [:install]
+end
+package "lzo" do
+     action [:install]
+end
+package "m4" do
+     action [:install]
+end
+package "mailx" do
+     action [:install]
+end
+package "make" do
+     action [:install]
+end
+package "MAKEDEV" do
+     action [:install]
+end
+package "mcstrans" do
+     action [:install]
+end
+package "mesa-libGL" do
+     action [:install]
+end
+package "mesa-libGL-devel" do
+     action [:install]
+end
+package "mod_ssl" do
+     action [:install]
+end
+package "mysql55" do
+     action [:install]
+end
+package "mysql55-devel" do
+     action [:install]
+end
+package "mysql55-libs" do
+     action [:install]
+end
+package "mysql55-server" do
+     action [:install]
+end
+package "neon" do
+     action [:install]
+end
+package "nginx" do
+     action [:install]
+end
+package "openssl-devel" do
+     action [:install]
+end
+package "openvpn" do
+     action [:install]
+end
+package "pango" do
+     action [:install]
+end
+package "pciutils" do
+     action [:install]
+end
+package "perl-DBD-MySQL" do
+     action [:install]
+end
+package "perl-DBI" do
+     action [:install]
+end
+package "php" do
+     action [:install]
+end
+package "php-cli" do
+     action [:install]
+end
+package "php-common" do
+     action [:install]
+end
+package "php-devel" do
+     action [:install]
+end
+package "php-fpm" do
+     action [:install]
+end
+package "php-gd" do
+     action [:install]
+end
+package "php-imap" do
+     action [:install]
+end
+package "php-mbstring" do
+     action [:install]
+end
+package "php-mcrypt" do
+     action [:install]
+end
+package "php-mysql" do
+     action [:install]
+end
+package "php-pdo" do
+     action [:install]
+end
+package "php-pear" do
+     action [:install]
+end
+package "php-pecl-apc" do
+     action [:install]
+end
+package "php-pecl-imagick" do
+     action [:install]
+end
+package "php-pecl-memcache" do
+     action [:install]
+end
+package "php-process" do
+     action [:install]
+end
+package "php-soap" do
+     action [:install]
+end
+package "php-xml" do
+     action [:install]
+end
+package "pkcs11-helper" do
+     action [:install]
+end
+package "policycoreutils" do
+     action [:install]
+end
+package "postgresql-libs" do
+     action [:install]
+end
+package "prelink" do
+     action [:install]
+end
+package "python-sqlite" do
+     action [:install]
+end
+package "readline-devel" do
+     action [:install]
+end
+package "selinux-policy" do
+     action [:install]
+end
+package "selinux-policy-targeted" do
+     action [:install]
+end
+package "setools" do
+     action [:install]
+end
+package "shared-mime-info" do
+     action [:install]
+end
+package "sharutils" do
+     action [:install]
+end
+package "subversion" do
+     action [:install]
+end
+package "sysvinit" do
+     action [:install]
+end
+package "t1lib" do
+     action [:install]
+end
+package "tcl" do
+     action [:install]
+end
+package "tcsh" do
+     action [:install]
+end
+package "ttmkfdir" do
+     action [:install]
+end
+package "udftools" do
+     action [:install]
+end
+package "urw-fonts" do
+     action [:install]
+end
+package "usermode" do
+     action [:install]
+end
+package "crontabs" do
+     action [:install]
+end
+package "xinetd" do
+     action [:install]
+end
+package "xml-commons-resolver" do
+     action [:install]
+end
+package "xml-commons-apis" do
+     action [:install]
+end
+package "xorg-x11-font-utils" do
+     action [:install]
+end
+package "xorg-x11-proto-devel" do
+     action [:install]
+end
+package "zlib-devel" do
+     action [:install]
+end
+
+execute "geoip" do
+  command "pecl install geoip"
+  action :run
+end
+
+cookbook_file "/etc/php.d/geoip.ini" do
+     source "etc/php.d/geoip.ini"
+     owner "root"
+     group "root"
+     mode 00644
+end
+
+cookbook_file "/usr/share/GeoIP/GeoIP.dat" do
+     source "usr/share/GeoIP/GeoIP.dat"
+     owner "root"
+     group "root"
+     mode 00644
+end
+
+cookbook_file "/usr/share/GeoIP/GeoIPCity.dat" do
+     source "usr/share/GeoIP/GeoIPCity.dat"
+     owner "root"
+     group "root"
+     mode 00644
+end
+
+cookbook_file "/usr/local/bin/s3dl" do
+     source "usr/local/bin/s3dl"
+     owner "root"
+     group "root"
+     mode 00755
+end
+
+execute "fixtime" do
+  command "ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime"
+  action :run
+end
